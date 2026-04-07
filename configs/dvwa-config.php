a7b63e01ddd407b50515f6579960a56db9d8667e
WILLIAM RODRIGO DOS SANTOS <114464205+Will2302@users.noreply.github.com>
2026-04-07 16:37:26 -0300

    Create dvwa-config.php

---
+++ configs/dvwa-config.php
@@ -0,0 +1,8 @@
+<?php
+$_DVWA = array();
+$_DVWA[ 'db_server' ] = '127.0.0.1';
+$_DVWA[ 'db_database' ] = 'dvwa';
+$_DVWA[ 'db_user' ] = 'dvwa';
+$_DVWA[ 'db_password' ] = 'p@ssw0rd';
+$_DVWA[ 'default_security_level' ] = 'low';
+?>
